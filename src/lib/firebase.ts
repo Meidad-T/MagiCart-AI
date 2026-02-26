6553303cc204a1395adbe2c527f1a0d5e31f88f0
Meidad Troper <oro27@txstate.edu>
2026-02-26 10:20:33 -0600

    secure env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4of7Cv83E0hLD7WmUw_bwN2fI2AXRZBU",
-  authDomain: "magicart-d27b7.firebaseapp.com",
-  projectId: "magicart-d27b7",
-  storageBucket: "magicart-d27b7.firebasestorage.app",
-  messagingSenderId: "731145279702",
-  appId: "1:731145279702:web:4d4086d6955e31536c1177",
-  measurementId: "G-7S0NKE4M24"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
